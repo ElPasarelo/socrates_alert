dbe15a46c98db7f5d897d6438f67116f51f1c402
Votre Nom <Vous@exemple.com>
2020-06-15 16:59:10 +0200

    first release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
-## [0.0.1] - TODO: Add release date.
+## [0.0.1] - 2020-06-15
 
 * TODO: Describe initial release.
+* This is just a test to learn how to create a package
--- lib/socratesalert.dart
+++ lib/socratesalert.dart
@@ -1,7 +1,42 @@
 library socratesalert;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class SocratesAlert {
+
+  static Future simple({ @required BuildContext context}) {
+    return showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular((15)))),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("Socrate reste dubitatif"),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image.network(
+                  "https://img.over-blog-kiwi.com/0/87/36/68/20160905/ob_df7021_socrates.jpg",
+                  height: 100,
+                  width: 100,
+                ),
+                Padding(padding: EdgeInsets.only(left: 5)),
+                Expanded(
+                  child: Text("Une erreur s'est produite, n'hésitez pas à relire toutes les informations"),
+                )
+              ],
+            ),
+            MaterialButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+      );
+    });
+  }
 }
+
